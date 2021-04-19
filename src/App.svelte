def38f214dc8e74116d33fac6b0dd75cc3752cc6
murermader <46532999+murermader@users.noreply.github.com>
2021-04-19 23:58:33 +0200

    Add UI form elements and wordlist

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,17 @@
 <script>
+  document.title = "Passphrase Generator";
+
+  let wordCount = 4;
+  let startsWithCapital = true;
+  let appendNumber = true;
+  let separationChar = "-";
+  export let wordlist = [];
+
+  let possibleWordCounts = [3, 4, 5, 6];
+
+  function generatePassphrase() {
+    console.log("Generate Passphrase");
+  }
 </script>
 
 <main class="md:flex justify-center w-full p-8 box-border">
@@ -6,18 +19,35 @@
     <h1 class="text-center">Passphrase Generator</h1>
     <div class="flex flex-col bg-gray-100 rounded-lg shadow-lg p-8 mt-4">
       <input type="text" class="mt-4" />
-      <div class="grid grid-cols-2 grid-rows-4 mt-6">
+      <div class="grid grid-cols-2 grid-rows-4 mt-4 items-center">
         <label for="word-count">Words</label>
-        <select name="" id="word-count">
-          <option value="3" />
-          <option value="4" />
-          <option value="5" />
-          <option value="6" />
+        <select bind:value={wordCount} id="word-count">
+          {#each possibleWordCounts as wordCount (wordCount)}
+            <option class={wordCount}>{wordCount}</option>
+          {/each}
         </select>
 
-        <div>3</div>
+        <label for="separation-char">Separation character</label>
+        <input maxlength="1" type="text" bind:value={separationChar} />
+
+        <label for="starts-with-capital">Starts with capital</label>
+        <input
+          bind:checked={startsWithCapital}
+          type="checkbox"
+          class="m-0 p-0"
+        />
+
+        <label for="starts-with-capital">Append number</label>
+        <input bind:checked={appendNumber} type="checkbox" class="m-0 p-0" />
       </div>
-      <button class="justify-self-end mt-4 bg-gray-50">Generate</button>
+      <button
+        on:click={generatePassphrase}
+        class="justify-self-end mt-4 bg-gray-50">New Password</button
+      >
     </div>
   </div>
+
+  <a class="absolute bottom-0 right-0 p-2 text-gray-500" href="#"
+    >Datenschutzerklärung</a
+  >
 </main>
